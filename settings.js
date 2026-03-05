7cd8d622653d96a76a5375134d632cab2c045202
silas454 <tagoesilas7@gmail.com>
2026-03-05 17:18:37 +0000

    Update GITHUB_USERNAME in settings.js

--- settings.js
+++ settings.js
@@ -1,21 +1,3 @@
-/**
- * Change the MASTER_PASSWORD to something secure
- * This password is required for:
- * - Pairing new sessions
- * - Accessing paired sessions via web
- */
+const GITHUB_USERNAME = "silas454";
 
-module.exports = {
-  MASTER_PASSWORD: "123456",
-  
-  // Session access code settings(don't change if you don't know what you're doing)
-  ACCESS_CODE_LENGTH: 6,
-  ACCESS_CODE_EXPIRY: 5 * 60 * 1000,
-  
-  // Server port
-  PORT: process.env.PORT || 3000,
-  
-  //Your GitHub username(make sure you've forked this bot without changing the name)
-  GITHUB_USERNAME: process.env.GITHUB_USERNAME || '',
- 
-};
+// Other settings...
